b1f1f57ce771b38b84960bc25ad2836470c07ba3
fu-jw <1151242757@qq.com>
2023-03-02 08:12:05 +0800

    整合knife4j(集成Swagger生成Api文档的增强解决方案)，并改造第一个接口

--- service-oa/src/main/java/com/fredo/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/fredo/auth/controller/SysRoleController.java
@@ -3,6 +3,8 @@ package com.fredo.auth.controller;
 import com.fredo.auth.service.SysRoleService;
 import com.fredo.common.result.Result;
 import com.fredo.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "角色管理接口")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -17,6 +20,7 @@ public class SysRoleController {
     @Autowired
     private SysRoleService sysRoleService;
 
+    @ApiOperation("获取全部角色列表")
     @GetMapping("findAll")
     public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
