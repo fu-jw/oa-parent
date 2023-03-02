b7b964fb234c20d95978da890882732ff16d2cf7
fu-jw <1151242757@qq.com>
2023-03-02 08:35:58 +0800

    条件分页查询

--- service-oa/src/main/java/com/fredo/auth/ServiceAuthApplication.java
+++ service-oa/src/main/java/com/fredo/auth/ServiceAuthApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.fredo")
-@MapperScan("com.fredo.*.mapper")
+//@MapperScan("com.fredo.*.mapper") 放到了 MybatisPlusConfig 中
 public class ServiceAuthApplication {
 
     public static void main(String[] args) {
--- service-oa/src/main/java/com/fredo/auth/controller/SysRoleController.java
+++ service-oa/src/main/java/com/fredo/auth/controller/SysRoleController.java
@@ -1,12 +1,18 @@
 package com.fredo.auth.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fredo.auth.service.SysRoleService;
 import com.fredo.common.result.Result;
 import com.fredo.model.system.SysRole;
+import com.fredo.vo.system.SysRoleQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +31,35 @@ public class SysRoleController {
     public Result<List<SysRole>> findAll() {
         List<SysRole> roleList = sysRoleService.list();
         return Result.ok(roleList);
+    }
+
+    /**
+     * 条件分页查询
+     * @param page  当前页
+     * @param limit 每页显示记录数
+     * @param sysRoleQueryVo    条件对象
+     * @return  条件分页查询结果
+     */
+    @ApiOperation("条件分页查询")
+    @GetMapping("{page}/{limit}")
+    public Result pageQueryRole(@PathVariable Long page,
+                                @PathVariable Long limit,
+                                SysRoleQueryVo sysRoleQueryVo) {
+        //调用service的方法实现
+        //1 创建Page对象，传递分页相关参数
+        //page 当前页  limit 每页显示记录数
+        Page<SysRole> pageParam = new Page<>(page,limit);
+
+        //2 封装条件，判断条件是否为空，不为空进行封装
+        LambdaQueryWrapper<SysRole> wrapper = new LambdaQueryWrapper<>();
+        String roleName = sysRoleQueryVo.getRoleName();
+        if(!StringUtils.isEmpty(roleName)) {
+            //封装 like模糊查询
+            wrapper.like(SysRole::getRoleName,roleName);
+        }
+
+        //3 调用方法实现
+        IPage<SysRole> pageModel = sysRoleService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
     }
 }
