51d64b891f4010627eee021b4f05c5cba20f4c3b
fu-jw <1151242757@qq.com>
2023-03-03 08:03:56 +0800

    后台管理--登录、获取信息、登出

---
+++ service-oa/src/main/java/com/fredo/auth/controller/IndexController.java
@@ -0,0 +1,51 @@
+package com.fredo.auth.controller;
+
+import com.fredo.common.result.Result;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Api("后台登录管理")
+@RestController
+@RequestMapping("/admin/system/index")
+public class IndexController {
+
+    /**
+     * 登录
+     * /admin/system/index/login
+     * post
+     */
+    @PostMapping("login")
+    public Result login(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("token","admin");
+        return Result.ok(map);
+    }
+
+    /**
+     * 获取用户信息
+     * @return
+     */
+    @GetMapping("info")
+    public Result info() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles","[admin]");
+        map.put("name","admin");
+        map.put("avatar","https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+        return Result.ok(map);
+    }
+
+    /**
+     * 退出
+     * @return
+     */
+    @PostMapping("logout")
+    public Result logout(){
+        return Result.ok();
+    }
+}
