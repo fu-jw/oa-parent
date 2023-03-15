ab8ae537aed77df393bb09c3dbdfc066c050b882
fu-jw <1151242757@qq.com>
2023-03-15 18:04:56 +0800

    流程处理--引入Activities 并简单功能测试

---
+++ service-oa/src/test/java/com/fredo/activiti/ProcessTest.java
@@ -0,0 +1,165 @@
+package com.fredo.activiti;
+
+import com.fredo.auth.ServiceAuthApplication;
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricTaskInstance;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipInputStream;
+
+@SpringBootTest(classes = ServiceAuthApplication.class)
+public class ProcessTest {
+
+    @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
+
+    /**
+     * 测试部署流程--单文件部署
+     */
+    @Test
+    public void deployTest() {
+        // 流程部署
+        Deployment deploy = repositoryService.createDeployment()
+                .addClasspathResource("process/qingjia.bpmn20.xml")
+                .addClasspathResource("process/qingjia.png")
+                .name("请假申请流程")
+                .deploy();
+        System.out.println("部署id：" + deploy.getId());
+        System.out.println("部署名：" + deploy.getName());
+    }
+
+    /**
+     * 测试部署流程--压缩文件部署
+     */
+    @Test
+    public void deployByZipTest() {
+        // 定义zip输入流
+        InputStream inputStream = this
+                .getClass()
+                .getClassLoader()
+                .getResourceAsStream(
+                        "process/qingjia.zip");
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+        // 流程部署
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zipInputStream)
+                .name("请假申请流程")
+                .deploy();
+        System.out.println("流程部署id：" + deployment.getId());
+        System.out.println("流程部署名称：" + deployment.getName());
+    }
+
+    /**
+     * 测试启动流程
+     */
+    @Test
+    public void startUpTest() {
+        //创建流程实例,我们需要知道流程定义的key
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("qingjia");
+        //输出实例的相关信息
+        System.out.println("流程定义id：" + processInstance.getProcessDefinitionId());
+        System.out.println("流程实例id：" + processInstance.getId());
+        System.out.println("当前活动Id：" + processInstance.getActivityId());
+    }
+
+    /**
+     * 测试查询当前个人待执行的任务
+     */
+    @Test
+    public void findPendingTaskList() {
+        //任务负责人
+        String assignee = "fjw";
+        List<Task> list = taskService.createTaskQuery()
+                .taskAssignee(assignee)//只查询该任务负责人的任务
+                .list();
+        for (Task task : list) {
+            System.out.println("流程实例id：" + task.getProcessInstanceId());
+            System.out.println("任务id：" + task.getId());
+            System.out.println("任务负责人：" + task.getAssignee());
+            System.out.println("任务名称：" + task.getName());
+        }
+    }
+
+    /**
+     * 完成任务
+     */
+    @Test
+    public void completeTask(){
+        Task task = taskService.createTaskQuery()
+                .taskAssignee("fjw")  //要查询的负责人
+                .singleResult();//返回一条
+
+        //完成任务,参数：任务id
+        taskService.complete(task.getId());
+    }
+
+    /**
+     * 查询已处理历史任务
+     */
+    @Test
+    public void findProcessedTaskList() {
+        //查询fjw已处理过的历史任务
+        List<HistoricTaskInstance> list = historyService
+                .createHistoricTaskInstanceQuery()
+                .taskAssignee("fjw").finished().list();
+        for (HistoricTaskInstance historicTaskInstance : list) {
+            System.out.println("流程实例id：" + historicTaskInstance.getProcessInstanceId());
+            System.out.println("任务id：" + historicTaskInstance.getId());
+            System.out.println("任务负责人：" + historicTaskInstance.getAssignee());
+            System.out.println("任务名称：" + historicTaskInstance.getName());
+        }
+    }
+
+    /**
+     * 查询流程定义
+     */
+    @Test
+    public void findProcessDefinitionList(){
+        List<ProcessDefinition> definitionList = repositoryService
+                .createProcessDefinitionQuery()
+                .orderByProcessDefinitionVersion()
+                .desc()
+                .list();
+        //输出流程定义信息
+        for (ProcessDefinition processDefinition : definitionList) {
+            System.out.println("流程定义 id="+processDefinition.getId());
+            System.out.println("流程定义 name="+processDefinition.getName());
+            System.out.println("流程定义 key="+processDefinition.getKey());
+            System.out.println("流程定义 Version="+processDefinition.getVersion());
+            System.out.println("流程部署ID ="+processDefinition.getDeploymentId());
+        }
+    }
+
+    /**
+     * 删除流程定义
+     */
+    @Test
+    public void deleteDeployment() {
+        //部署id
+        String deploymentId = "a602c1da-c303-11ed-b052-9840bb0b2cdd";
+        //删除流程定义，如果该流程定义已有流程实例启动则删除时出错
+        repositoryService.deleteDeployment(deploymentId);
+        //设置true 级联删除流程定义，即使该流程有流程实例启动也可以删除，设置为false非级别删除方式
+        //repositoryService.deleteDeployment(deploymentId, true);
+    }
+
+
+}
