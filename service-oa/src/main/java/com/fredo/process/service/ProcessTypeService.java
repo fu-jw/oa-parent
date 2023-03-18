a4b9ddaaa8fbb827219f076de2d5099aba09c31f
fu-jw <1151242757@qq.com>
2023-03-18 21:37:13 +0800

    获取全部审批分类及模板

--- service-oa/src/main/java/com/fredo/process/service/ProcessTypeService.java
+++ service-oa/src/main/java/com/fredo/process/service/ProcessTypeService.java
@@ -3,6 +3,9 @@ package com.fredo.process.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.fredo.model.process.ProcessType;
 
+import java.util.List;
+
 public interface ProcessTypeService extends IService<ProcessType> {
 
+    List<ProcessType> findProcessType();
 }
--- service-oa/src/main/java/com/fredo/process/service/impl/ProcessTypeServiceImpl.java
+++ service-oa/src/main/java/com/fredo/process/service/impl/ProcessTypeServiceImpl.java
@@ -1,13 +1,43 @@
 package com.fredo.process.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fredo.model.process.ProcessTemplate;
 import com.fredo.process.mapper.ProcessTypeMapper;
+import com.fredo.process.service.ProcessTemplateService;
 import com.fredo.process.service.ProcessTypeService;
 import com.fredo.model.process.ProcessType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class ProcessTypeServiceImpl extends ServiceImpl<ProcessTypeMapper, ProcessType> implements ProcessTypeService {
 
+    @Autowired
+    private ProcessTemplateService processTemplateService;
+
+    @Override
+    public List<ProcessType> findProcessType() {
+        //1 查询所有审批分类，返回list集合
+        List<ProcessType> processTypeList = baseMapper.selectList(null);
+
+        //2 遍历返回所有审批分类list集合
+        for (ProcessType processType : processTypeList) {
+            //3 得到每个审批分类，根据审批分类id查询对应审批模板
+            //审批分类id
+            Long typeId = processType.getId();
+            //根据审批分类id查询对应审批模板
+            LambdaQueryWrapper<ProcessTemplate> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(ProcessTemplate::getProcessTypeId, typeId);
+            List<ProcessTemplate> processTemplateList = processTemplateService.list(wrapper);
+
+            //4 根据审批分类id查询对应审批模板数据（List）封装到每个审批分类对象里面
+            processType.setProcessTemplateList(processTemplateList);
+        }
+
+        return processTypeList;
+    }
 }
