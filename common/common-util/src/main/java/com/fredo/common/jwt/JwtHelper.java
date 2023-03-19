54564df9e998404a812c0661b757c2519f7a8943
fu-jw <1151242757@qq.com>
2023-03-19 20:28:28 +0800

    获取全部审批分类及模板

--- common/common-util/src/main/java/com/fredo/common/jwt/JwtHelper.java
+++ common/common-util/src/main/java/com/fredo/common/jwt/JwtHelper.java
@@ -64,10 +64,10 @@ public class JwtHelper {
             return null;
         }
     }
-//    public static void main(String[] args) {
-//        String token = JwtHelper.createToken(1L, "admin");
-//        System.out.println(token);
+    public static void main(String[] args) {
+        String token = JwtHelper.createToken(1L, "admin");
+        System.out.println(token);
 //        System.out.println(JwtHelper.getUserId(token));
 //        System.out.println(JwtHelper.getUsername(token));
-//    }
+    }
 }
